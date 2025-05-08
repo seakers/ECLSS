314a1c47dbd98a0d24a41ea249b4e32701b9ee97
Mahima <mahima333@tamu.edu>
2025-05-08 13:27:25 -0500

    chnage values after 10 sec

--- simulation.py
+++ simulation.py
@@ -17,41 +17,8 @@ json_file_path = 'jsonfile/sim_data.json'
 # url = 'https://daphne-at-lab.selva-research.com/api/at/receiveHeraFeed'
 url = 'http://localhost:8002/api/at/receiveHeraFeed'
 
-# Define control ranges (safe limits) for monitored parameters
-CONTROL_RANGES = {
-    "ppO2": {"min": 21.4, "max": 21.6},  # kPa
-    "ppCO2": {"min": 0.39, "max": 0.41},   # kPa
-    "humidity": {"min": 10, "max": 120},    # liters
-    "temperature": {"min": 18, "max": 26},  # °C
-}
 
-MONITOR_RANGES = {
-    "ppO2": {"min": 19.5, "max": 23.5},  # kPa
-    "ppCO2": {"min": 0.0, "max": 0.5},   # kPa
-    "humidity": {"min": 10, "max": 120},    # liters
-    "temperature": {"min": 18, "max": 26},  # °C
-}
-
-# Define failure scenarios (easily configurable)
-FAILURE_SCENARIOS = [
-    {
-        "subsystem": "CDRS",           # Subsystem to fail
-        "failure_step": 2000,            # Time step when the failure occurs
-        "recovery_step": 6000,           # Time step when the subsystem recovers (None if not recoverable)
-        "failure_mode": "off",         # Failure mode: 'off' (completely stop) or 'reduced' (partial degradation)
-        "reduction_factor": 0.0,       # Reduction factor (used only if failure_mode is 'reduced')
-    },
-    # Add additional failure scenarios as needed
-    # {
-    #     "subsystem": "OGS",
-    #     "failure_step": 50,
-    #     "recovery_step": 80,
-    #     "failure_mode": "reduced",
-    #     "reduction_factor": 0.5,
-    # },
-]
-
-PARAMETER_INFO = {
+PARAMETER_INFO1 = {
     "ppO2": {"DisplayName": "Cabin_ppO2", "Id": 43, "ParameterGroup": "L1", "NominalValue": 163.81,
              "UpperCautionLimit": 175.0, "UpperWarningLimit": 185.0, "LowerCautionLimit": 155.0,
              "LowerWarningLimit": 145.0, "Divisor": 100, "Name": "ppO2", "Unit":"mmHg"},
@@ -61,15 +28,31 @@ PARAMETER_INFO = {
     "humidity": {"DisplayName": "Humidity", "Id": 45, "ParameterGroup": "L1", "NominalValue": 52,
               "UpperCautionLimit": 61, "UpperWarningLimit": 70, "LowerCautionLimit": 50,
               "LowerWarningLimit": 40, "Divisor": 1, "Name": "Humidity", "Unit":"L"},
-    "ppO21": {"DisplayName": "Cabin_ppO2", "Id": 43, "ParameterGroup": "L2", "NominalValue": 163.81,
+    "ppO21": {"DisplayName": "Cabin_ppO2", "Id": 46, "ParameterGroup": "L2", "NominalValue": 163.81,
              "UpperCautionLimit": 175.0, "UpperWarningLimit": 185.0, "LowerCautionLimit": 155.0,
              "LowerWarningLimit": 145.0, "Divisor": 100, "Name": "ppO2", "Unit":"mmHg"},
-    "ppCO21": {"DisplayName": "Cabin_ppCO2", "Id": 44, "ParameterGroup": "L2", "NominalValue": 0.4,
+    "ppCO21": {"DisplayName": "Cabin_ppCO2", "Id": 47, "ParameterGroup": "L2", "NominalValue": 0.4,
               "UpperCautionLimit": 4.5, "UpperWarningLimit": 6.0, "LowerCautionLimit": -1.0,
               "LowerWarningLimit": -2.0, "Divisor": 100, "Name": "ppCO2", "Unit":"mmHg"},
-    "humidity1": {"DisplayName": "Humidity", "Id": 45, "ParameterGroup": "L2", "NominalValue": 52,
+    "humidity1": {"DisplayName": "Humidity", "Id": 48, "ParameterGroup": "L2", "NominalValue": 52,
               "UpperCautionLimit": 61, "UpperWarningLimit": 70, "LowerCautionLimit": 50,
               "LowerWarningLimit": 40, "Divisor": 1, "Name": "Humidity", "Unit":"L"},
+    "H2O": {"DisplayName": "H2O", "Id": 49, "ParameterGroup": "Crew", "NominalValue": 3.6,
+              "UpperCautionLimit": 4.5, "UpperWarningLimit": 5.0, "LowerCautionLimit": 2.5,
+              "LowerWarningLimit": 2.0, "Divisor": 1, "Name": "H2O", "Unit":"L"},
+    "CabinTemperature": {"DisplayName": "Cabin Temperature", "Id": 49, "ParameterGroup": "L1", "NominalValue": 72.3,
+              "UpperCautionLimit": 79.0, "UpperWarningLimit": 87.7, "LowerCautionLimit": 68,
+              "LowerWarningLimit": 64, "Divisor": 1, "Name": "Cabin Temperature", "Unit":"L"},
+    "CabinTemperature1": {"DisplayName": "Cabin Temperature", "Id": 49, "ParameterGroup": "L2", "NominalValue": 72.3,
+              "UpperCautionLimit": 79.0, "UpperWarningLimit": 87.7, "LowerCautionLimit": 68,
+              "LowerWarningLimit": 64, "Divisor": 1, "Name": "Cabin Temperature", "Unit":"L"},
+     "SOXIE": {"DisplayName": "SOXIE Stack Temp", "Id": 49, "ParameterGroup": "", "NominalValue": 1481,
+              "UpperCautionLimit": 1629, "UpperWarningLimit": 1670, "LowerCautionLimit": 1333,
+              "LowerWarningLimit": 1292, "Divisor": 1, "Name": "SOXIE Stack Temp", "Unit":"L"},
+    "MOXIE": {"DisplayName": "MOXIE Compressor Temp", "Id": 49, "ParameterGroup": "", "NominalValue": 160,
+              "UpperCautionLimit": 170, "UpperWarningLimit": 180, "LowerCautionLimit": 140,
+              "LowerWarningLimit": 120, "Divisor": 1, "Name": "MOXIE Compressor Temp", "Unit":"L"},
+    
     
     # "temperature": {"DisplayName": "Cabin_Temperature", "Id": 46, "ParameterGroup": "L1", "NominalValue": 100,
     #           "UpperCautionLimit": 79.0, "UpperWarningLimit": 87.7, "LowerCautionLimit": 68.0,
@@ -79,54 +62,56 @@ PARAMETER_INFO = {
     #           "LowerWarningLimit": 64.0, "Divisor": 1, "Name": "Cabin Temperature", "Unit":"F"},
 }
 
-# Simulate over time
-time_steps = 10000
-json_data = {"ppO2": [], "ppCO2": [], "humidity": [], "temperature": [], "OGS": [], "CDRS": [], "WRS": [], "TCS": []}
-history = {"ppO2": [], "ppCO2": [], "humidity": [], "temperature": [], "ppO21": [], "ppCO21": [], "humidity1": [], "temperature1": [],}
-status_history = {"OGS": [], "CDRS": [], "WRS": [], "TCS": []}
+# 
+
+PARAMETER_INFO = {
+    "ppO2": PARAMETER_INFO1["ppO2"],
+    "ppCO2": PARAMETER_INFO1["ppCO2"],
+    "ppO21": PARAMETER_INFO1["ppO21"],
+    "ppCO21": PARAMETER_INFO1["ppCO21"],
+    # "H2O": PARAMETER_INFO1["H2O"],
+    "humidity": PARAMETER_INFO1["humidity"],
+    "humidity1": PARAMETER_INFO1["humidity1"],
+    # "CabinTemperature": PARAMETER_INFO1["CabinTemperature"],
+    # "CabinTemperature1": PARAMETER_INFO1["CabinTemperature1"],
+    # "SOXIE": PARAMETER_INFO1["SOXIE"],
+    # "MOXIE": PARAMETER_INFO1["MOXIE"],
+}
 
 cabin = {
-    "ppO2": 150, 
-    "ppCO2": 5, 
-    "humidity": 65, 
-    "temperature": 22.0, 
+    "ppO2": 165, 
+    "ppCO2": 0.5, 
+    "humidity": 52, 
+    # "CabinTemperature": 80.0, 
      "ppO21": 165, 
     "ppCO21": 0.5, 
     "humidity1": 52, 
-    "temperature1": 52.0,  
+    # "CabinTemperature1": 80.0, 
+    # "H2O": 3.5,
+    # "SOXIE": 1480,
+    # "MOXIE": 172,
 }
 
-
-
+# Simulate over time
+time_steps = 10000
+counter = [0]
 def simulate_step(cabin):
     """
     Simulates one time step in the ECLSS system, applying failures and subsystem dynamics.
     """
-    # Apply failure scenarios at the current step
-    # subsystems = apply_failures(subsystems, failure_scenarios, current_step)
+    if counter[0] > 10:
+        cabin = {
+            "ppO2": 165, 
+            "ppCO2": 6, 
+            "humidity": 52, 
+            "ppO21": 165, 
+            "ppCO21": 6, 
+            "humidity1": 52, 
+   
+        }
+        return cabin
 
-    # # Human respiration effects
-    # cabin = human_respiration_effect(cabin, respiration, time_step)
-
-    # # Human water consumption
-    # water_consumed = human_water_consumption(cabin, respiration, time_step)
-
-    # # WRS: Recover water
-    # cabin = water_recovery_system(cabin, subsystems, water_consumed, time_step)
-
-    # Subsystems effects
-    # OGS: Generates O2, consumes water (if on)
-    # if subsystems["OGS"]["status"] and cabin["water"] > subsystems["OGS"]["water_consumption"] * time_step:
-    #     O2_generated = subsystems["OGS"]["O2_rate"] * time_step
-    #     cabin["ppO2"] += (O2_generated * MOLAR_VOLUME) / CABIN_VOLUME
-    #     cabin["water"] -= subsystems["OGS"]["water_consumption"] * time_step
-
-    # # CDRS: Removes CO2 (if on)
-    # if subsystems["CDRS"]["status"]:
-    #     CO2_removed = subsystems["CDRS"]["CO2_removal_rate"] * time_step
-    #     cabin["ppCO2"] -= (CO2_removed * MOLAR_VOLUME) / CABIN_VOLUME
-    #     cabin["ppCO2"] = max(cabin["ppCO2"], 0)  # Prevent negative ppCO2
-
+    counter[0] += 1
     return cabin
 
 def post_json_to_url():
@@ -148,15 +133,6 @@ def post_json_to_url():
         else:
             print("Failed:", response.status_code)
             # print(f"Response content: {response.text}")
-    # except requests.exceptions.ConnectionError:
-    #     print("Connection Error: Could not connect to the server. Is it running?")
-    #     return False
-    # except requests.exceptions.Timeout:
-    #     print("Timeout: The server took too long to respond.")
-    #     return False
-    # except json.JSONDecodeError:
-    #     print("Error: Invalid JSON data in the file.")
-    #     return False
     except KeyboardInterrupt:
         print("Stopped by user.")
     except Exception as e:
@@ -176,15 +152,9 @@ def create_parameter_entry(param_name, cabin):
     param_info = PARAMETER_INFO[param_name]
     current_value = cabin[param_name]
 
-    # Determine display value and unit
-    # if param_name == "ppO2" or param_name == "ppCO2":
-    #     current_value_display = round(current_value * 7.50062, 2)  # kPa to mmHg
-    # else:
-    #     
     current_value_display = current_value
     unit = param_info["Unit"]
 
-    # Create the parameter entry
     parameter_entry = {
         "SimulatedParameter": True,
         "DisplayName": param_info["DisplayName"],
@@ -231,8 +201,6 @@ def create_json():
     file_name = f"sim_data.json"
     file_path = os.path.join(folder_path, file_name)
 
-    # Convert ppO2 to the required format (assuming it's in kPa and needs to be in mmHg)
-    # ppO2_mmHg = round(cabin["ppO2"] * 7.50062, 2)  # Conversion factor from kPa to mmHg
 
     parameters_list = [create_parameter_entry(param_name, cabin) for param_name in PARAMETER_INFO]
 
@@ -264,19 +232,10 @@ for t in range(time_steps):
 
     # subsystems = check_limits_and_control(cabin, subsystems)
     cabin = simulate_step(cabin)
-    
-    # Store history
-    history["ppO2"].append(cabin["ppO2"])
-    history["ppCO2"].append(cabin["ppCO2"])
-    history["humidity"].append(cabin["humidity"])
-
+  
     # Save telemetry data
     create_json()
-    
-    # Send data (uncomment when `post2url` function is defined)
-    # post2url(jsonfile)
-
-    # Control execution speed if real_time_mode is enabled
+   
     if real_time_mode:
         elapsed_time = time.time() - start_time
         sleep_time = max(0, (1.0 / simulation_speed) - elapsed_time)  # Ensure non-negative sleep time
